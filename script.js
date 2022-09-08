f4e2c1429f207e00ec0c3a60ebb419ac99ad5634
thizzy824 <agpastora7@gmail.com>
2022-09-07 17:43:31 -0700

    created questions, answers, and hints

--- script.js
+++ script.js
@@ -1,8 +1,34 @@
 console.log('yadadaaaaa')
 
 /*----- constants -----*/
+// Questions for the game
 
+const quest1 = 'What city are the 49ers from?'
+const quest2 = 'How many rings does Tom Brady have?'
+const quest3 = 'Which player is known as "The Sheriff"?'
+const quest4 = 'What name did player Chad Johnson legally change his name to?'
+const quest5 = 'What is Deion Sanders iconic nickname?'
+const quest6 = 'What is the Buffalo Bills fan-base known as?'
+const quest7 = 'Who is the GOAT?'
 
+// Answers for game
+const ans1 = 'San Francisco'
+const ans2 = 7
+const ans3 = 'Peyton Manning'
+const ans4 = 'Ocho Cinco'
+const ans5 = 'Prime Time'
+const ans6 = 'Bills Mafia'
+const ans7 = 'Tom Brady'
+
+// Hints for game
+
+const hint1 = 'Golden Gate Bridge'
+const hint2 = '1 more than Jordan'
+const hint3 = 'One of the Manning brothers'
+const hint4 = '#85'
+const hint5 = 'Game title'
+const hint6 = 'The Godfather series'
+const hint7 = '6th round pick, #199th overall'
 
 /*----- app's state (variables) -----*/
 
