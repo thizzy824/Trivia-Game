8e88c84f5d1a01202d0bf3ac3eff8b790a2770cd
thizzy824 <agpastora7@gmail.com>
2022-09-08 15:03:26 -0700

    reformatted html and css

--- script.js
+++ script.js
@@ -20,23 +20,11 @@ const ans5 = 'Prime Time'
 const ans6 = 'Bills Mafia'
 const ans7 = 'Tom Brady'
 
-// Hints for game
 
-const hint1 = 'Golden Gate Bridge'
-const hint2 = '1 more than Jordan'
-const hint3 = 'One of the Manning brothers'
-const hint4 = '#85'
-const hint5 = 'Game title'
-const hint6 = 'The Godfather series'
-const hint7 = '6th round pick, #199th overall'
 
 /*----- app's state (variables) -----*/
 // Variables grabbed from HTML
-let score = document.querySelector('.score')
 
-let highScore = document.querySelector('.high-score')
-
-let playButton = document.querySelector('.play')
 
 
 
