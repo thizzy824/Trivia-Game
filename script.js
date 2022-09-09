cf18cc557dbd12d8d500667442ca7b483b3a9f70
thizzy824 <agpastora7@gmail.com>
2022-09-08 22:01:16 -0700

    added questions and answer options to array

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ console.log('yadadaaaaa')
 const quest1 = 'What city are the 49ers from?'
 const quest2 = 'How many rings does Tom Brady have?'
 const quest3 = 'Which player is known as "The Sheriff"?'
-const quest4 = 'What name did player Chad Johnson legally change his name to?'
+const quest4 = 'Which player legally changed his name to "OchoCinco" to match his jersey number? (#85)'
 const quest5 = 'What is Deion Sanders iconic nickname?'
 const quest6 = 'What is the Buffalo Bills fan-base known as?'
 const quest7 = 'Who is the GOAT?'
@@ -15,7 +15,7 @@ const quest7 = 'Who is the GOAT?'
 const ans1 = 'San Francisco'
 const ans2 = 7
 const ans3 = 'Peyton Manning'
-const ans4 = 'Ocho Cinco'
+const ans4 = 'Chad Johnson'
 const ans5 = 'Prime Time'
 const ans6 = 'Bills Mafia'
 const ans7 = 'Tom Brady'
@@ -45,15 +45,63 @@ let questions = [
         option2: 'New York',
         option3: 'Denver',
         option4: 'Las Vegas',
-    }
+        answer: 1,
+    },
+    {
+        question: quest2,
+        option1: 5,
+        option2: 6,
+        option3: ans2,
+        option4: 8,
+        answer: 3,
+    },
+    {
+        question: quest3,
+        option1: 'Brett Favre',
+        option2: 'Patrick Mahomes',
+        option3: 'Joe Burrow',
+        option4: ans3,
+        answer: 4,
+    },
+    {
+        question: quest4,
+        option1: 'Randy Moss',
+        option2: ans4,
+        option3: 'Tony Gonzalez',
+        option4: 'Antonio Gates',
+        answer: 2,
+    },
+    {
+        question: quest5,
+        option1: 'The Play-Maker',
+        option2: 'Megatron',
+        option3: ans5,
+        option4: 'Weapon X',
+        answer: 3,
+    },
+    {
+        question: quest6,
+        option1: ans6,
+        option2: ' Forever Faithful',
+        option3: 'The 12th Man',
+        option4: "America's Team",
+        answer: 1,
+    },
+    {
+        question: quest7,
+        option1: 'Joe Montana',
+        option2: 'Bo Jackson',
+        option3: 'Lawrence Taylor',
+        option4: ans7,
+        answer:4,
+    },
 ]
 
-
-
-
-
-
 /*----- cached element references -----*/
+// point system
+const pointsAwarded = 1000
+// set total number of questions
+const totalQuestions = 7
 
 
 
