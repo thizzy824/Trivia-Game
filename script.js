b55785c9af6f9bf4179baedbcb79367bcf258206
thizzy824 <agpastora7@gmail.com>
2022-09-08 20:04:48 -0700

    created more variables and arrays for js

--- script.js
+++ script.js
@@ -24,6 +24,31 @@ const ans7 = 'Tom Brady'
 
 /*----- app's state (variables) -----*/
 // Variables grabbed from HTML
+const question = document.querySelector('#question')
+// use an array method array.from that creates new array from answers
+const answers = document.querySelectorAll('.answer-text')
+const currentLevel = document.querySelector('#current-text')
+const scoreCount = document.querySelector('#score')
+// create empty object
+let currentQuestion = {}
+// variables that will change/be updated
+let takeAnswers = true
+let score = 0
+let questionNumber = 0
+let remainingQuestions = []
+
+// create array and object with questions and answer options
+let questions = [
+    {
+        question: quest1,
+        option1: ans1,
+        option2: 'New York',
+        option3: 'Denver',
+        option4: 'Las Vegas',
+    }
+]
+
+
 
 
 
