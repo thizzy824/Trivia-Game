4049696938fb6cddca75cbe1a36e5eed8a8ffa3f
thizzy824 <agpastora7@gmail.com>
2022-09-10 16:49:00 -0700

    reworked js to fix bug and cleaned up code

--- script.js
+++ script.js
@@ -3,35 +3,34 @@
 /*----- constants -----*/
 // Questions for the game
 
-const quest1 = 'What city are the 49ers from?';
-const quest2 = 'How many rings does Tom Brady have?';
-const quest3 = 'Which player is known as "The Sheriff"?';
-const quest4 =
-	'Which player legally changed his name to "OchoCinco" to match his jersey number? (#85)';
-const quest5 = 'What is Deion Sanders iconic nickname?';
-const quest6 = 'What is the Buffalo Bills fan-base known as?';
-const quest7 = 'Who is the GOAT?';
+// 'What city are the 49ers from?';
+// 'How many rings does Tom Brady have?';
+// 'Which player is known as "The Sheriff"?';
+// 'Which player legally changed his name to "OchoCinco" to match his jersey number? (#85)';
+// 'What is Deion Sanders iconic nickname?';
+// 'What is the Buffalo Bills fan-base known as?';
+// 'Who is known as the GOAT?';
 
 // Answers for game
-const ans1 = 'San Francisco';
-const ans2 = 7;
-const ans3 = 'Peyton Manning';
-const ans4 = 'Chad Johnson';
-const ans5 = 'Prime Time';
-const ans6 = 'Bills Mafia';
-const ans7 = 'Tom Brady';
+// 1 = 'San Francisco';
+// 2 = 7;
+// 3 = 'Peyton Manning';
+// 4 = 'Chad Johnson';
+// 5 = 'Prime Time';
+// 6 = 'Bills Mafia';
+// 7 = 'Tom Brady';
 
 /*----- app's state (variables) -----*/
 // Variables grabbed from HTML
 const question = document.querySelector('#question');
 // use an array method array.from that creates new array from answers
-const answers = document.querySelectorAll('.answer-text');
+const answers = Array.from(document.querySelectorAll('.answer-text'));
 const currentLevel = document.querySelector('#currentText');
 const scoreCount = document.querySelector('#score');
 // create empty object
 let currentQuestion = {};
 // variables that will change/be updated
-let takeAnswers = true;
+let takingAnswers = true;
 let score = 0;
 let questionNumber = 0;
 let remainingQuestions = [];
@@ -39,68 +38,69 @@ let remainingQuestions = [];
 // create array and object with questions and answer options
 let questions = [
 	{
-		question: quest1,
-		option1: ans1,
+		question: 'What city are the 49ers from?',
+		option1: 'San Francisco',
 		option2: 'New York',
 		option3: 'Denver',
 		option4: 'Las Vegas',
 		answer: 1,
 	},
 	{
-		question: quest2,
-		option1: 5,
-		option2: 6,
-		option3: ans2,
-		option4: 8,
+		question: 'How many rings does Tom Brady have?',
+		option1: '5',
+		option2: '6',
+		option3: '7',
+		option4: '8',
 		answer: 3,
 	},
 	{
-		question: quest3,
+		question: 'Which player is known as "The Sheriff"?',
 		option1: 'Brett Favre',
 		option2: 'Patrick Mahomes',
 		option3: 'Joe Burrow',
-		option4: ans3,
+		option4: 'Peyton Manning',
 		answer: 4,
 	},
 	{
-		question: quest4,
+		question:
+			'Which player legally changed his name to "OchoCinco" to match his jersey number? (#85)',
 		option1: 'Randy Moss',
-		option2: ans4,
+		option2: 'Chad Johnson',
 		option3: 'Tony Gonzalez',
 		option4: 'Antonio Gates',
 		answer: 2,
 	},
 	{
-		question: quest5,
+		question: 'What is Deion Sanders iconic nickname?',
 		option1: 'The Play-Maker',
 		option2: 'Megatron',
-		option3: ans5,
+		option3: 'Prime Time',
 		option4: 'Weapon X',
 		answer: 3,
 	},
 	{
-		question: quest6,
-		option1: ans6,
+		question: 'What is the Buffalo Bills fan-base known as?',
+		option1: 'Bills Mafia',
 		option2: ' Forever Faithful',
 		option3: 'The 12th Man',
 		option4: "America's Team",
 		answer: 1,
 	},
 	{
-		question: quest7,
+		question: 'Who is known as the GOAT?',
 		option1: 'Joe Montana',
 		option2: 'Bo Jackson',
 		option3: 'Lawrence Taylor',
-		option4: ans7,
+		option4: 'Tom Brady',
 		answer: 4,
 	},
 ];
 
 /*----- cached element references -----*/
 // point system
-const pointsAwarded = 1000;
+const POINTS_AWARDED = 1000;
 // set total number of questions
-const totalQuestions = 7;
+const TOTAL_QUESTIONS = 7;
 
 /*----- functions -----*/
 // create function to play game
@@ -111,12 +111,12 @@ playGame = () => {
 	// [...] get all values (in this case objects)in an array
 	// https://codeburst.io/what-are-three-dots-in-javascript-6f09476b03e1
 	remainingQuestions = [...questions];
-	nextQuestion();
+	getNextQuestion();
 };
 // create function to prompt next question
 getNextQuestion = () => {
 	// if there are 0 remaining questions-save last score
-	if (remainingQuestions.length === 0 || questionNumber > totalQuestions) {
+	if (remainingQuestions.length === 0 || questionNumber > TOTAL_QUESTIONS) {
 		// https://developer.mozilla.org/en-US/docs/Web/API/Storage/setItem
 		// use setItem method of storage interface when passed a key name/value
 		localStorage.setItem('lastScore', score);
@@ -127,58 +127,58 @@ getNextQuestion = () => {
 	// increase question number if there are remaining questions
 	questionNumber++;
 	//  change text to display current level by using method .innerText
-	currentLevel.innerText = `Level ${questionNumber} / ${totalQuestions}`;
+	currentLevel.innerText = `Level ${questionNumber} / ${TOTAL_QUESTIONS}`;
 	// randomize questions and set next question text
 	const questionsIndex = Math.floor(Math.random() * remainingQuestions.length);
 	// keep track of what question player is currently on
 	currentQuestion = remainingQuestions[questionsIndex];
 	question.innerText = currentQuestion.question;
 	// forEach method to apply to all answer text
-	
-	answers.forEach((option) => {
-		const number = answers.dataset['num'];
+
+	answers.forEach(option => {
+		const number = option.dataset['num'];
 		option.innerText = currentQuestion['option' + number];
 	});
-	
+
 	//  remove elements from the remainingQuestions array using splice method
 	remainingQuestions.splice(questionsIndex, 1);
-	
-	takeAnswers = true;
+
+	takingAnswers = true;
 };
 
 /*----- event listeners -----*/
 
-answers.forEach(option => {
+answers.forEach((option) => {
 	option.addEventListener('click', e => {
 		// if the game isnt taking answers, function ends
-		if (!takeAnswers) {
-			return
+		if (!takingAnswers) {
+			return;
 		}
 		// set variable made earlier takeAnswers to false
-		takeAnswers = false
-		const optionChosen = e.target
-		const answerChosen = optionChosen.dataset['num']
+		takingAnswers = false;
+		const optionChosen = e.target;
+		const answerChosen = optionChosen.dataset['num'];
 		// apply styling class to right/wrong answers using conditional ternary operators
 		// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Conditional_Operator
-		let classBeingApplied = answerChosen == currentQuestion.answer ? 'correct' : 'incorrect'
+		let classBeingApplied = answerChosen == currentQuestion.answer ? 'right' : 'wrong';
 		// create another conditional to increase score
-		if (classBeingApplied === 'correct') {
-			awardPoints(pointsAwarded)
+		if (classBeingApplied === 'right') {
+			awardPoints(POINTS_AWARDED);
 		}
 		// add classBeingApplied to optionChosen using classList.add method
-		optionChosen.parentElement.classList.add(classBeingApplied)
+		optionChosen.parentElement.classList.add(classBeingApplied);
 		// remove class added after a set amount of time using setTimeout method and classlist.remove method
 		setTimeout(() => {
-			optionChosen.parentElement.classList.remove(classBeingApplied)
+			optionChosen.parentElement.classList.remove(classBeingApplied);
 			// run getNextQuestion function to proceed to next question after timer set
-			getNextQuestion()
-		}, 1000)
-	})
-})
+			getNextQuestion();
+		}, 1000);
+	});
+});
 
 awardPoints = points => {
-	score += points
-	scoreCount.innerText = score
-}
+	score += points;
+	scoreCount.innerText = score;
+};
 
-playGame()
+playGame();
